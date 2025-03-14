052760187775a68a9e38e84871026fed97d1d27c
Dylan Cheng <83380918+dylanc316@users.noreply.github.com>
2025-03-14 16:53:45 -0500

    unzipgit

---
+++ covoisino-dev/lib/core/services.dart
@@ -0,0 +1,18 @@
+// services.dart
+
+// This will be implemented later with actual Firebase/API calls
+class AuthService {
+  // Authentication methods will go here
+}
+
+class UserService {
+  // User-related API calls will go here
+}
+
+class VehicleService {
+  // Vehicle-related API calls will go here
+}
+
+class RideService {
+  // Ride-related API calls will go here
+}
